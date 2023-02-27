413702b6adf11a4d21bf8b31bfea7e73df3de550
Parsaebrahimi2004 <126461561+Parsaebrahimi2004@users.noreply.github.com>
2023-02-27 13:27:15 +0330

    Add files via upload
    this code will help you to calculate your score

---
+++ mohasebe noomre.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+int x;
+Console.WriteLine("Enter the score");
+x=Convert.ToInt32(Console.ReadLine());
+if (x > 20)
+{
+    Console.WriteLine("namosan chejori bishtar az 20 gerefti ?!");
+}
+else if (x >= 17)
+{
+    Console.WriteLine("Congrats! you earned rank A ");
+}
+else if (x >= 15)
+{
+    Console.WriteLine("Not bad but you can do better. you earned rank B");
+}
+else if (x >= 10)
+{
+    Console.WriteLine("Well at least you passed. you earned rank C");
+}
+else if (x < 10)
+{
+    Console.WriteLine("I have nothing to say ... you FAILED");
+}
+int percentComplete = (int)Math.Round((double)(100 * x) / 20);
+Console.WriteLine("Your score in percentage : "); Console.WriteLine(percentComplete);
